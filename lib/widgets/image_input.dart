8fe67ac777c2f6e9948b6e450d5db89846c857ed
luongductinh-it <ldt20062003@gmail.com>
2026-01-14 08:22:29 +0700

    Update project

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  void _takePicture() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2),
+        ),
+      ),
+      height: 250,
+      alignment: Alignment.center,
+      width: double.infinity,
+      child: TextButton.icon(
+        onPressed: _takePicture,
+        icon: Icon(Icons.camera),
+        label: Text('Take Picture'),
+      ),
+    );
+  }
+}
